f1bb756e03ab1738be72146026e1be12c1ae0553
Uday Theja Madisetty <um2162@columbia.edu>
2021-10-06 22:55:13 +0000

    Filter movies by rating

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,13 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    logger.debug "params #{params}"
+    @all_ratings = Movie.all_ratings
+    @ratings_to_show = []
+    if params.key? "ratings"
+        @ratings_to_show = params["ratings"].keys
+    end
+    @movies = Movie.with_ratings(@ratings_to_show)
   end
 
   def new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,14 @@
 class Movie < ActiveRecord::Base
+
+  def self.all_ratings
+    self.uniq.pluck(:rating)
+  end
+
+  def self.with_ratings(ratings_list)
+    logger.debug "Rating list #{ratings_list}"
+    if ratings_list.nil? or ratings_list.empty?
+      return self.all
+    end
+    self.where(rating: ratings_list)
+  end
 end
